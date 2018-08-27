7c19a4d026b658eb6d6033b13a52278d03d8d3f9
wilton <wilton@wilton>
2018-08-27 18:02:35 -0300

    Exercicios2_Inicio

--- exercicio2.php
+++ exercicio2.php
@@ -10,15 +10,71 @@
  Exercício 2
 Assumindo o exemplo fictício de uma locadora, modele e construa os códigos
 necessários para as seguintes classes de um sistema:
-Classe Usuario: deve conter o nome, telefone e endereço do usuário da locadora, e
+
+ * Classe Usuario: deve conter o nome, telefone e endereço do usuário da locadora, e
 também o número de filmes que ele já locou. Deve conter um método que exiba estas
 informações.
 
  * Classe Funcionario: deve conter o nome, telefone e endereço do funcionário da
 locadora, e o valor do seu salário. Deve conter um método que exiba estas
 informações.
+ */
+  
+   echo "Exercício 2";
+   echo '<br><br>';
+   
+   class Pessoa{
+       
+       public $nome;
+       public $endereco;
+       public $telefone;
+       
+       public function __construct($nome, $endereco, $telefone) {          
+           $this->setNome($nome);
+           $this->setEndereco($endereco);
+           $this->setTelefone($telefone);           
+       }
+       
+       private function setNome($nome){
+           $this->nome = $nome;
+       }
+       
+       private function setEndereco($endereco){
+           $this->endereco = $endereco;
+       }
+       
+       private function setTelefone($telefone){
+           $this->telefone = $telefone;
+       }
+       
+       public function getNome(){
+           return $this->nome;
+       }
+       
+       public function getTelefone() {
+           return $this->telefone;
+       }
+       
+       public function getEdereco() {
+           return $this->endereco;
+       }
+   }
+   
+   class Usuario extends Pessoa{
+       public $numerosDeFilmesAlocados;
+       
+       public function __construct($nome, $endereco, $telefone) {
+           parent::__construct($nome, $endereco, $telefone);
+       }
+       
+       public function mostraInformacoes() {
+           
+           
+       }      
+       
+   }
 
- * Classe CD: deve conter o nome do artista, o nome do álbum e estilo. Deve conter um
+ /* Classe CD: deve conter o nome do artista, o nome do álbum e estilo. Deve conter um
 método que exiba estas informações.
 
  * Classe DVD: deve conter o título do DVD e o estilo. Deve conter um método que exiba
@@ -41,12 +97,8 @@ de código e manter o seu código organizado. Utilize as recomendações apresen
 em aula, como por exemplo, modificadores de acessos private, protecte e public
 quando necessários, bem como o uso de métodos getters e setters.
  */
-    echo "Exercício 2";
-    echo '<br><br>';
-
     
-    
-    
+        
     
 ?>
     </BODY>
